94a3f8f22b5aa870d07817d2eff1c0b93c15a3cc
shivam kachhadiya <94698146+shivamkachhadiya@users.noreply.github.com>
2025-03-02 22:18:59 +0530

    Add files via upload

---
+++ parameter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void fun(){
+    int a=3;
+    int b=5;
+    cout<<a+b<<endl;
+}
+int main(){
+    for(int i=0;i<=5;i++){
+        fun();
+
+    }
+    
+
+  
+    
+}
+
